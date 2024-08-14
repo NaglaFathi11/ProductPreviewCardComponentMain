a9ee97135c9a7c7cae40c3cfad8b038276ed55ba
NaglaFathi11 <naglafatehi11@gmail.com>
2024-08-14 18:29:59 +0200

    w

--- src/Components/ImgSection/ImgSection.jsx
+++ src/Components/ImgSection/ImgSection.jsx
@@ -1,18 +1,17 @@
-import '../ImgSection/ImgSection.css'
-import ImgContainerDesktop from '../../assets/Images/image-product-desktop.jpg'
-import ImgContainerMobile from '../../assets/Images/image-product-mobile.jpg'
-
+import "../ImgSection/ImgSection.css";
+import ImgContainerDesktop from "/assets/Images/ImgContainerDesktop";
+import ImgContainerMobile from "/assets/Images/ImgContainerMobile";";
 
 function ImageContainer() {
-    return (
-        <div id="PictureContainer">
-        <picture>
-            <source media="(max-width: 768px)" srcSet={ImgContainerMobile} />
-            <source media="(min-width: 769px)" srcSet={ImgContainerDesktop} />
-            <img src={ImgContainerDesktop} id='PhotoDesktop' alt="not-found"/>
-        </picture>
-        </div>
-    )
+  return (
+    <div id="PictureContainer">
+      <picture>
+        <source media="(max-width: 768px)" srcSet={ImgContainerMobile} />
+        <source media="(min-width: 769px)" srcSet={ImgContainerDesktop} />
+        <img src={ImgContainerDesktop} id="PhotoDesktop" alt="not-found" />
+      </picture>
+    </div>
+  );
 }
 
-export default ImageContainer
+export default ImageContainer;
